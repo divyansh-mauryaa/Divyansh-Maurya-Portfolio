56d63af247aa597ae0dcf39d40a7fbdf6203304d
divyansh-mauryaa <143538157+divyansh-mauryaa@users.noreply.github.com>
2023-08-30 15:09:41 +0530

    Update NavbarStyledComponent.js

--- src/components/Navbar/NavbarStyledComponent.js
+++ src/components/Navbar/NavbarStyledComponent.js
@@ -86,7 +86,7 @@ export const GitHubButton = styled.a`
   font-weight: 500;
   text-decoration: none;
   font-size: 16px;
-  transition: all 0.6s ease-in-out;
+  transition: 0.6s;
     :hover {
       background: ${({ theme }) => theme.primary};
       color: ${({ theme }) => theme.white};     
@@ -95,6 +95,30 @@ export const GitHubButton = styled.a`
     font-size: 14px;
     }
 `;
+
+export const FiverrButton = styled.a`
+  border: 1.8px solid ${({ theme }) => theme.primary};
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  height: 70%;
+  border-radius: 20px;
+  color: ${({ theme }) => theme.primary};
+  cursor: pointer;
+  padding: 0 20px;
+  font-weight: 500;
+  text-decoration: none;
+  font-size: 16px;
+  transition: 0.6s;
+    :hover {
+      background: green;
+      color: ${({ theme }) => theme.white};     
+    }
+    @media screen and (max-width: 768px) { 
+    font-size: 14px;
+    }
+`;
+
 
 export const ButtonContainer = styled.div`
   width: 80%;  
